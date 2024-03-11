d9ccd5de03658e6d0b608f8b4bdbdc8d6c07e249
AntonioScl <antoniosclocchi@gmail.com>
2024-03-11 10:57:26 +0100

    formatting

--- scripts/dataset_sample-forw_back.py
+++ scripts/dataset_sample-forw_back.py
@@ -21,16 +21,21 @@ from guided_diffusion.script_util import (
 from guided_diffusion.image_datasets import load_data, _list_images_per_classes
 import datetime
 import pickle
+
 # from torchvision.utils import save_image
 from PIL import Image
+
 # from more_itertools import ilen
 import time
+
 
 def main():
     args = create_argparser().parse_args()
 
     dist_util.setup_dist()
-    output_images = os.path.join(args.output, f"t_{args.step_reverse}_{args.timestep_respacing}_images")
+    output_images = os.path.join(
+        args.output, f"t_{args.step_reverse}_{args.timestep_respacing}_images"
+    )
     logger.configure(dir=output_images)
 
     logger.log("creating model and diffusion...")
@@ -45,11 +50,12 @@ def main():
         model.convert_to_fp16()
     model.eval()
 
-   
     # output_images = os.path.join(logger.get_dir(), f"t_{args.step_reverse}_{args.timestep_respacing}_images")
 
     logger.log("creating data loader...")
-    list_images = _list_images_per_classes(args.data_dir, args.num_per_class, args.num_classes, output_images)
+    list_images = _list_images_per_classes(
+        args.data_dir, args.num_per_class, args.num_classes, output_images
+    )
     num_samples = len(list_images)
     data_start = load_data(
         data_dir=args.data_dir,
@@ -60,7 +66,7 @@ def main():
         random_crop=False,
         random_flip=False,
         list_images=list_images,
-        drop_last=False,   # It is important when batch_size < num_samples, otherwise it doesn't yield
+        drop_last=False,  # It is important when batch_size < num_samples, otherwise it doesn't yield
     )
 
     # num_samples = len(num_samples)
@@ -79,29 +85,37 @@ def main():
         # logger.log(f"batch loaded: {batch_start.shape}")
 
         labels_start = extra["y"].to(dist_util.dev())
-        batch_start  = batch_start.to(dist_util.dev())
-        img_names    = extra["img_name"]
+        batch_start = batch_start.to(dist_util.dev())
+        img_names = extra["img_name"]
         # Sample noisy images from the diffusion process at time t_reverse given by the step_reverse argument
-        t_reverse = diffusion._scale_timesteps(th.tensor([args.step_reverse])).to(dist_util.dev())
+        t_reverse = diffusion._scale_timesteps(th.tensor([args.step_reverse])).to(
+            dist_util.dev()
+        )
         # t_reverse = t_reverse.to(dist_util.dev())
-        batch_noisy = diffusion.q_sample(batch_start, t_reverse) if args.step_reverse<int(args.timestep_respacing) else th.randn(batch_start.shape, device=dist_util.dev())
+        batch_noisy = (
+            diffusion.q_sample(batch_start, t_reverse)
+            if args.step_reverse < int(args.timestep_respacing)
+            else th.randn(batch_start.shape, device=dist_util.dev())
+        )
         logger.log("completed forward diffusion...")
 
         model_kwargs = {}
         if args.class_cond:
-            classes = labels_start # Condition the diffusion on the labels of the original images
+            classes = labels_start  # Condition the diffusion on the labels of the original images
             # classes = th.randint(
             #     low=0, high=NUM_CLASSES, size=(args.batch_size,), device=dist_util.dev()
             # )
             model_kwargs["y"] = classes
 
         sample_fn = (
-            diffusion.p_sample_loop_forw_back if not args.use_ddim else diffusion.ddim_sample_loop_forw_back
+            diffusion.p_sample_loop_forw_back
+            if not args.use_ddim
+            else diffusion.ddim_sample_loop_forw_back
         )
         sample = sample_fn(
             model,
             (len(batch_start), 3, args.image_size, args.image_size),
-            step_reverse = args.step_reverse,  # step when to reverse the diffusion process
+            step_reverse=args.step_reverse,  # step when to reverse the diffusion process
             noise=batch_noisy,
             clip_denoised=args.clip_denoised,
             model_kwargs=model_kwargs,
@@ -112,9 +126,18 @@ def main():
         sample = sample.contiguous()
 
         # Save the images
-        real_t_reverse = int(t_reverse.item()) if diffusion.rescale_timesteps else int(t_reverse.item() * (1000.0 / float(args.timestep_respacing)))
+        real_t_reverse = (
+            int(t_reverse.item())
+            if diffusion.rescale_timesteps
+            else int(t_reverse.item() * (1000.0 / float(args.timestep_respacing)))
+        )
         for ii in range(len(sample)):
-            name = img_names[ii].split('.')[0] + '_t' +'{:04d}'.format(real_t_reverse) + '.JPEG'
+            name = (
+                img_names[ii].split(".")[0]
+                + "_t"
+                + "{:04d}".format(real_t_reverse)
+                + ".JPEG"
+            )
             img = Image.fromarray(np.array(sample[ii].cpu()).astype(np.uint8))
             img.save(os.path.join(output_images, name))
             # save_image(sample[ii], os.path.join(output_images, name))
@@ -126,7 +149,6 @@ def main():
         # gathered_samples = [th.zeros_like(sample) for _ in range(dist.get_world_size())]
         # dist.all_gather(gathered_samples, sample)  # gather not supported with NCCL
         # all_images.extend([sample.cpu().numpy() for sample in gathered_samples])
-
 
         # if args.class_cond:
         #     gathered_labels = [
@@ -153,8 +175,9 @@ def main():
         sample_size = th.tensor(len(sample)).to(dist_util.dev())
         dist.all_reduce(sample_size, op=dist.ReduceOp.SUM)
         generated_samples += sample_size.item()
-        logger.log(f"created {generated_samples} samples in {time.time() - time_start:.1f} seconds")
-        
+        logger.log(
+            f"created {generated_samples} samples in {time.time() - time_start:.1f} seconds"
+        )
 
     # arr = np.concatenate(all_images, axis=0)
     # arr = arr[: num_samples]
@@ -168,13 +191,20 @@ def main():
     if dist.get_rank() == 0:
         # Save the arguments of the run
         date_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f")
-        out_args = os.path.join(args.output, f"t_{args.step_reverse}_{args.timestep_respacing}_args_{date_time}.pk")
+        out_args = os.path.join(
+            args.output,
+            f"t_{args.step_reverse}_{args.timestep_respacing}_args_{date_time}.pk",
+        )
         logger.log(f"saving args to {out_args}")
-        with open(out_args, 'wb') as handle: pickle.dump(args, handle)
+        with open(out_args, "wb") as handle:
+            pickle.dump(args, handle)
 
         # Save the time it took to generate the samples
-        out_time = os.path.join(args.output, f"t_{args.step_reverse}_{args.timestep_respacing}_timing.txt")
-        with open(out_time, 'a') as f: f.write(f"{generated_samples} \t {time.time() - time_start:.3f}\n")
+        out_time = os.path.join(
+            args.output, f"t_{args.step_reverse}_{args.timestep_respacing}_timing.txt"
+        )
+        with open(out_time, "a") as f:
+            f.write(f"{generated_samples} \t {time.time() - time_start:.3f}\n")
 
         # # Save the data of the run
         # shape_str = "x".join([str(x) for x in arr.shape])
@@ -198,21 +228,25 @@ def create_argparser():
         model_path="",
     )
     defaults.update(model_and_diffusion_defaults())
-    defaults.update(dict(
-        step_reverse = 10,
-        data_dir =  'datasets/ILSVRC2012/validation',
-        output  =  os.path.join(os.getcwd(),
-             'results',
-             'diffused_ILSVRC2012_validation'),
-        num_per_class = 10,
-        num_classes = 10,
-    ))
+    defaults.update(
+        dict(
+            step_reverse=10,
+            data_dir="datasets/ILSVRC2012/validation",
+            output=os.path.join(
+                os.getcwd(), "results", "diffused_ILSVRC2012_validation"
+            ),
+            num_per_class=10,
+            num_classes=10,
+        )
+    )
     parser = argparse.ArgumentParser()
     add_dict_to_argparser(parser, defaults)
 
     assert parser.parse_args().step_reverse >= 0, "step_reverse must be positive"
-    assert parser.parse_args().step_reverse <= int(parser.parse_args().timestep_respacing), "step_reverse must be smaller than or equal to timestep_respacing"
- 
+    assert parser.parse_args().step_reverse <= int(
+        parser.parse_args().timestep_respacing
+    ), "step_reverse must be smaller than or equal to timestep_respacing"
+
     return parser
 
 
